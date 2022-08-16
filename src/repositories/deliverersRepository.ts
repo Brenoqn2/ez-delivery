5e34498e503cfef83a50bd9b27dfdf27e464bc7e
Brenoqn2 <brenoqn2@gmail.com>
2022-08-16 13:10:26 -0300

    heroku deploy

--- src/repositories/deliverersRepository.ts
+++ src/repositories/deliverersRepository.ts
@@ -34,9 +34,44 @@ async function getDelivererByPhone(phone: string) {
   return deliverer;
 }
 
+async function getAvailableDeliverers(userId: number) {
+  const deliverers = await prisma.deliverers.findMany({
+    where: {
+      userId,
+      available: true,
+    },
+  });
+  return deliverers;
+}
+
+async function setDelivererNotAvailable(id: number) {
+  await prisma.deliverers.update({
+    where: {
+      id,
+    },
+    data: {
+      available: false,
+    },
+  });
+}
+
+async function setDelivererAvailable(id: number) {
+  await prisma.deliverers.update({
+    where: {
+      id,
+    },
+    data: {
+      available: true,
+    },
+  });
+}
+
 const deliverersRepository = {
   register,
   getDelivererByEmail,
   getDelivererByPhone,
+  getAvailableDeliverers,
+  setDelivererNotAvailable,
+  setDelivererAvailable,
 };
 export default deliverersRepository;
